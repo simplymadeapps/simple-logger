4d1e5ffa6d31dc647eba0efa36f5aa6630e5d1ba
Bill Burgess <bill@simplymadeapps.com>
2017-07-25 15:35:30 -0500

    Add base logger class
    Clean up line spacing in AppDelegate.
    Clean up example ViewController.

---
+++ SimpleLogger/SimpleLogger.h
@@ -0,0 +1,13 @@
+//
+//  SimpleLogger.h
+//  SimpleLogger
+//
+//  Created by Bill Burgess on 7/25/17.
+//  Copyright © 2017 Simply Made Apps Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleLogger : NSObject
+
+@end
---
+++ SimpleLogger/SimpleLogger.m
@@ -0,0 +1,13 @@
+//
+//  SimpleLogger.m
+//  SimpleLogger
+//
+//  Created by Bill Burgess on 7/25/17.
+//  Copyright © 2017 Simply Made Apps Inc. All rights reserved.
+//
+
+#import "SimpleLogger.h"
+
+@implementation SimpleLogger
+
+@end
